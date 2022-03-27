ce6cd3d661d318b4272d0aa97ab8c465a611b408
khahoang <hoangkha2910@gmail.com>
2022-03-27 23:44:54 +0700

    update --create Expenses component and edit css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import ExpenseItem from "./component/ExpenseItem";
+import Expenses from "./component/Expenses";
 
 function App() {
   const expense = [
@@ -28,26 +28,9 @@ function App() {
 
   return (
     <div>
-      <h2>Student list</h2>
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      />
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      />
-      <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      />
-      <ExpenseItem
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
+      <h2>Let's get started!</h2>
+      <Expenses
+        items={expense} 
       />
     </div>
   );
